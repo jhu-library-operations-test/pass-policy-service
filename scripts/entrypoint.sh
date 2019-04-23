4b60f0a1dda80594783cfa635d55f758edd404f2
Aaron Birkland <apb@jhu.edu>
2019-04-23 16:02:41 -0400

    Add docker-appropriate policies dsl

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,4 +4,4 @@ printf "\n**** Begin Environment Variable Dump ****\n\n"
 printenv | sort
 printf "\n**** End Environment Variable Dump ****\n\n"
 
-./pass-policy-service serve
+./pass-policy-service serve ${POLICY_FILE}
